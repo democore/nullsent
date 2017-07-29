1254904d0d1dc304e603e521752d7d6880b457c5
yuripiano <eirika.sawh@gmail.com>
2017-07-29 16:09:23 +0900

    changed movement speed for test character

--- Assets/Scripts/CharacterMovemend2D.cs
+++ Assets/Scripts/CharacterMovemend2D.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class CharacterMovemend2D : MonoBehaviour {
 
-	// Use this for initialization hi bearcore
-	void Start () {
-		
+    CharacterController controller;
+    [Range(0.01f, 1f)]
+    public float speed = 0.1f;
+    // Use this for initialization hi bearcore
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        //controller.Move();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal != 0f)
+        {
+            controller.Move(new Vector3(horizontal * speed, 0f, 0f));
+        }
+    }
 }
