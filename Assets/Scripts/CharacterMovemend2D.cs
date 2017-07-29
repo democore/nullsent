f72e29b810d3bbfddfbfdf806430ea9c637aedb2
democore <n.brettschneider@gmx.de>
2017-07-29 08:50:38 +0200

    fixed bug

--- Assets/Scripts/CharacterMovemend2D.cs
+++ Assets/Scripts/CharacterMovemend2D.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class CharacterMovemend2D : MonoBehaviour {
 
 	// Use this for initialization hi bearcore
 	void Start () {
