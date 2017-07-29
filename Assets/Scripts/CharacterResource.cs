bb94fa65238a1ffdd178123997e187b3fe79006d
democore <n.brettschneider@gmx.de>
2017-07-29 22:20:04 +0200

    Added door

--- Assets/Scripts/CharacterResource.cs
+++ Assets/Scripts/CharacterResource.cs
@@ -17,7 +17,7 @@ public class CharacterResource : MonoBehaviour {
     bool didDie = false;
 
     //Yuri's variables <3
-    List<Light> LightsGhostIsIn = new List<Light>(); //list of lights the ghost is in 
+    Light CurrentLight; //list of lights the ghost is in 
                                                      //atm restricted to 1 light, but could change
     public float dimAmount = 0.5f;  //how much the light the ghost is in dims every update
                                     //could be changed so that each light has its own amount it dims by
@@ -65,36 +65,33 @@ public class CharacterResource : MonoBehaviour {
         }
         else
         {
-            foreach (Light toDim in LightsGhostIsIn)
+            float intensityToChange = CurrentLight.intensity;
+            if (intensityToChange != 0f)
             {
-                float intensityToChange = toDim.intensity;
-                if (intensityToChange != 0f )
+                if (Resource < MaxResource)
                 {
-                    if(Resource < MaxResource)
+                    //each light could have its own "dimming factor" so that dif lights could dim at dif speeds
+                    intensityToChange -= dimAmount;
+                    CurrentLight.intensity = intensityToChange;
+                    //Resource += curRegen;
+                    resourceChange = curRegen;
+                    if (float.IsNaN(soundDelay) || soundDelay <= Time.time)
                     {
-                        //intensityToChange -= toDim.dimFactor; 
-                        //each light could have its own "dimming factor" so that dif lights could dim at dif speeds
-                        intensityToChange -= dimAmount;
-                        toDim.intensity = intensityToChange;
-                        //Resource += curRegen;
-                        resourceChange = curRegen;
-                        if (float.IsNaN(soundDelay) || soundDelay <= Time.time)
-                        {
-                            AudioManager.Instance.PlayEffect("Powerup");
-                            soundDelay = Time.time + 1f;
-                        }
+                        AudioManager.Instance.PlayEffect("Powerup");
+                        soundDelay = Time.time + 1f;
+                    }
 
-                        collectingSystem.Play();
+                    collectingSystem.Play();
 
-                        //Stop from going into loop if it's empty
-                        if (intensityToChange <= 0f)
-                        {
-                            Destroy(toDim.GetComponent<Collider>());
-                            InLight = false;
-                        }
+                    //Stop from going into loop if it's empty
+                    if (intensityToChange <= 0f)
+                    {
+                        Destroy(CurrentLight.GetComponent<Collider>());
+                        InLight = false;
                     }
                 }
             }
+
         }
 
         Resource += resourceChange;
@@ -129,7 +126,7 @@ public class CharacterResource : MonoBehaviour {
         Light l = other.GetComponent<Light>();
         if (l != null && l.type == LightType.Spot)
         {
-            LightsGhostIsIn.Add(l);
+            CurrentLight = l;
             InLight = true;
         }
     }
@@ -138,7 +135,10 @@ public class CharacterResource : MonoBehaviour {
     {
         Light l = other.GetComponent<Light>();
         if (l != null && l.type == LightType.Spot)
+        {
+            CurrentLight = null;
             InLight = false;
+        }
     }
 
     private void setCharacterIntensity(float i)
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    public string NextLevelName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+
+        }
+    }
+}
