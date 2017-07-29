63cae53f9ec839474774986d5ce5ef7188adb045
Skiastra <annacraig1455@hotmail.com>
2017-07-29 16:30:28 -0400

    Fonts, fades and textures

---
+++ Assets/Scripts/UI/ScreenFade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFade : Utility.Singleton<ScreenFade> {
+
+    public Animator anim;
+
+    void FadeIn()
+    {
+        anim.SetTrigger("FadeIn");
+    }
+
+    void FadeOut()
+    {
+        anim.SetTrigger("FadeOut");
+    }
+}
